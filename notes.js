745810dd385fbe91ebd38332f4464fa9f9cca699
vishdha <vishaldhayagude07@gmail.com>
2018-02-02 09:47:46 +0530

    [new] Add Note data

--- notes.js
+++ notes.js
@@ -1,7 +1,28 @@
 console.log('Starting note.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) =>  {
-	console.log('Adding note', title, body);
+	var notes = [];
+	var note = {
+		title,
+		body
+	};
+
+	try{
+		var notesString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(notesString);
+	} catch(e) {
+
+	}
+
+	//ES6 
+	var duplicateNotes = notes.filter((note) => note.title === title);
+
+	if (duplicateNotes.length === 0){
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	}
 };
 
 var getAll = () => {
